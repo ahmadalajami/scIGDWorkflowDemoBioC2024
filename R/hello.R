723bfdd21d90754a88371d397f9fcf0608fff22d
Ahmad Al Ajami <ahmad.alajami@kgu.de>
2024-05-23 14:55:45 +0200

    Added 'hello' function

---
+++ R/hello.R
@@ -0,0 +1,13 @@
+#' Say hello
+#'
+#' This function is just an example function to pass R CMD check.
+#'
+#' @return A `character(1)` with the word "hello".
+#' @export
+#'
+#' @examples
+#' ## Say hello ^_^
+#' hello()
+hello <- function() {
+    return("hello")
+}
