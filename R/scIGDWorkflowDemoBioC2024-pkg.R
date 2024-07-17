f1ce991e150c1a43ab97f595260e9a41caedfc53
Federico Marini <marinif@uni-mainz.de>
2024-07-17 15:44:40 +0200

    defining import statements and info on the package

---
+++ R/scIGDWorkflowDemoBioC2024-pkg.R
@@ -0,0 +1,17 @@
+#' scIGDWorkflowDemoBioC2024
+#'
+#' @importFrom scaeData scaeDataGet
+#' @importFrom SingleCellAlleleExperiment read_allele_counts
+#' @importFrom scran modelGeneVar
+#' @importFrom scater plotReducedDim
+#' @importFrom org.Hs.eg.db org.Hs.eg.db
+#' @importFrom ggplot2 ggplot
+#' @importFrom cowplot plot_grid
+#' @importFrom Matrix readMM
+#' @importFrom igraph cluster_louvain
+#' @importFrom iSEE iSEE
+#'
+#' @name scIGDWorkflowDemoBioC2024-pkg
+#' @docType package
+#' @keywords internal
+"_PACKAGE"
